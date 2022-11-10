962337a72f0a2031e40b5fc41263455489450a0f
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-10 16:03:09 +0100

    email

---
+++ logic.js
@@ -0,0 +1,19 @@
+// Chiedere all'utente la sua email e controllare se questa è presente nella lsita email
+
+
+
+// creare lista email
+const emailList = ["gio@gmail.it", "fra@gmail.it", "vin@gmail.it"];
+console.log(emailList);
+
+// chiedere all'utente la sua email
+let userEmail = prompt("Inserisci la tua email");
+console.log(userEmail)
+
+// stampare un messaggio per l'utente
+if (userEmail === emailList){
+    console.log("Sei invitato");
+} else{
+    console.log("Non sei invitato");
+}
+
